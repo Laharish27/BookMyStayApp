118b344b4a07d54097e47da79af4654563e3265a
Laharish27 <laharishkondaveeti2007@gmail.com>
2026-03-06 14:25:52 +0530

    feature/UC1-WelcomePage

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,6 @@
+public class BookMyStayApp {
+    public static void main(String[] args){
+        System.out.println("Welcome To The Hotel Booking Management");
+        System.out.println("System Initialized Successfully");
+    }
+}
